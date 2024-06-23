e3533e7585e9e1be6f315be28b94b6fbaa8cb036
theendercore <theendercore@gmail.com>
2024-06-23 15:00:41 +0300

    - Torch params got flipped :)

--- src/main/java/architectspalette/content/blocks/CustomTorchBlock.java
+++ src/main/java/architectspalette/content/blocks/CustomTorchBlock.java
@@ -16,15 +16,15 @@ public class CustomTorchBlock extends TorchBlock {
     protected final Supplier<SimpleParticleType> flameGetter;
 
     public CustomTorchBlock(Properties properties, Supplier<SimpleParticleType> particleGetter) {
-        super(properties, ParticleTypes.FLAME);
+        super(ParticleTypes.FLAME, properties);
         flameGetter = particleGetter;
     }
 
     @Override
     public void animateTick(BlockState p_57494_, Level p_57495_, BlockPos p_57496_, RandomSource p_57497_) {
-        double d0 = (double)p_57496_.getX() + 0.5D;
-        double d1 = (double)p_57496_.getY() + 0.7D;
-        double d2 = (double)p_57496_.getZ() + 0.5D;
+        double d0 = (double) p_57496_.getX() + 0.5D;
+        double d1 = (double) p_57496_.getY() + 0.7D;
+        double d2 = (double) p_57496_.getZ() + 0.5D;
         p_57495_.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
         p_57495_.addParticle(this.flameGetter.get(), d0, d1, d2, 0.0D, 0.0D, 0.0D);
     }
