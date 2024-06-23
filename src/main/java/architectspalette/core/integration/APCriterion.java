09b310b2167c16df5cdc3b05bb8781efdb69246f
theendercore <theendercore@gmail.com>
2024-06-23 21:03:07 +0300

    - CriteriaTriggers registering also changed (I just didn't think to check for some reason)

--- src/main/java/architectspalette/core/integration/APCriterion.java
+++ src/main/java/architectspalette/core/integration/APCriterion.java
@@ -1,14 +1,19 @@
 package architectspalette.core.integration;
 
 import architectspalette.core.integration.advancement.CarveTotemTrigger;
-import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.CriterionTrigger;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+
+import static architectspalette.core.ArchitectsPalette.rl;
 
 public class APCriterion {
 
-    public static CarveTotemTrigger CARVE_TOTEM = new CarveTotemTrigger();
+    public static CarveTotemTrigger CARVE_TOTEM = register(rl("carve_totem"),new CarveTotemTrigger());
 
-    public static void register() {
-        CriteriaTriggers.register(CARVE_TOTEM);
+    //(ender) I don't know if there is a forge registry for this
+    public static <T extends CriterionTrigger<?>> T register(ResourceLocation name, T criterion) {
+        return Registry.register(BuiltInRegistries.TRIGGER_TYPES, name, criterion);
     }
-
 }
