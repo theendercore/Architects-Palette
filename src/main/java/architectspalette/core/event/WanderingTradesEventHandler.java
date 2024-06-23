2b4d5c9cfbee706518e986d1cc709816bc9ae645
theendercore <theendercore@gmail.com>
2024-06-23 16:41:22 +0300

    - BasicItemListing (gone) now using ItemsForEmeralds

--- src/main/java/architectspalette/core/event/WanderingTradesEventHandler.java
+++ src/main/java/architectspalette/core/event/WanderingTradesEventHandler.java
@@ -4,8 +4,8 @@ import architectspalette.core.ArchitectsPalette;
 import architectspalette.core.config.APConfig;
 import architectspalette.core.registry.APBlocks;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.VillagerTrades.ItemsForEmeralds;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.BasicItemListing;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,9 +19,9 @@ public class WanderingTradesEventHandler {
     public static void onWanderingTradesLoaded(WandererTradesEvent event) {
         if (APConfig.WANDERER_TRADES_ENABLED.get()) {
             List<VillagerTrades.ItemListing> generic = event.getGenericTrades();
-
-            generic.add(new BasicItemListing(2, new ItemStack(APBlocks.SUNSTONE.get(), 6), 20, 2, 0f));
-            generic.add(new BasicItemListing(2, new ItemStack(APBlocks.MOONSTONE.get(), 6), 20, 2, 0f));
+            // stack, price, count, max uses, xp, multiplier (or I hope it's this)
+            generic.add(new ItemsForEmeralds(new ItemStack(APBlocks.SUNSTONE.get()), 2, 6, 20, 2, 0f));
+            generic.add(new ItemsForEmeralds(new ItemStack(APBlocks.MOONSTONE.get()), 2, 6, 20, 2, 0f));
         }
     }
 }
