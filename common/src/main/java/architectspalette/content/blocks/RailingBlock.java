440a774b68e83946d4de8d380a29be79ab43bad7
theendercore <theendercore@gmail.com>
2024-06-25 11:13:12 +0300

    - moved all already platform independent code from "content" to common

--- common/src/main/java/architectspalette/content/blocks/RailingBlock.java
+++ common/src/main/java/architectspalette/content/blocks/RailingBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CrossCollisionBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.FluidState;
--- common/src/main/java/architectspalette/content/worldgen/features/APTreeGrowers.java
+++ common/src/main/java/architectspalette/content/worldgen/features/APTreeGrowers.java
@@ -1,6 +1,5 @@
 package architectspalette.content.worldgen.features;
 
-import architectspalette.core.ArchitectsPalette;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.grower.TreeGrower;
@@ -8,7 +7,9 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 
 import java.util.Optional;
 
+import static architectspalette.core.APConstants.modLoc;
+
 public class APTreeGrowers {
-    private static final ResourceKey<ConfiguredFeature<?, ?>> key = ResourceKey.create(Registries.CONFIGURED_FEATURE, ArchitectsPalette.rl("twisted_tree"));
+    private static final ResourceKey<ConfiguredFeature<?, ?>> key = ResourceKey.create(Registries.CONFIGURED_FEATURE, modLoc("twisted_tree"));
     public static final TreeGrower WARPED_TREE = new TreeGrower("warped",Optional.empty(), Optional.of(key), Optional.empty());
 }
