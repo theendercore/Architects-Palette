cd1fae0d22d9c28b1655b28d4f97c0e5124a4b6f
theendercore <theendercore@gmail.com>
2024-06-25 21:49:54 +0300

    - re-wrote RegistryUtils.createBlock

--- common/src/main/java/architectspalette/core/registry/util/RegistryUtils.java
+++ common/src/main/java/architectspalette/core/registry/util/RegistryUtils.java
@@ -3,16 +3,20 @@ package architectspalette.core.registry.util;
 import architectspalette.core.platform.Services;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
 import static architectspalette.core.APConstants.mcLoc;
 
 public class RegistryUtils {
-    // (ender) yes forge at's the Tab reg keys, and not im not doing that, this ref is fine
+    // (ender) yes forge AT's the Tab reg keys, and not im not doing that, this ref is fine
     static final ResourceKey<CreativeModeTab> INGREDIENTS_TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, mcLoc("ingredients"));
+    static final ResourceKey<CreativeModeTab> BUILDING_BLOCKS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, mcLoc("building_blocks"));
 
     public static Item resourceItem() {
         return new Item(new Item.Properties());
@@ -26,31 +30,30 @@ public class RegistryUtils {
         return Services.REGISTRY.registerItem(name, item, INGREDIENTS_TAB);
     }
 
-    public static Supplier<Item> createItem(String name, Supplier<Item> item, ResourceKey<CreativeModeTab> tab) {
+    @SafeVarargs
+    public static Supplier<Item> createItem(String name, Supplier<Item> item, @Nullable ResourceKey<CreativeModeTab>... tab) {
         return Services.REGISTRY.registerItem(name, item, tab);
     }
 
+    public static Supplier<Block> createBlock(String name, Supplier<Block> block) {
+        return createBlock(name, block, BUILDING_BLOCKS);
+    }
 
-//	public static <B extends Block> RegistryObject<B> createBlock(String name, Supplier<? extends B> supplier) {
-//		return createBlock(name, supplier, CreativeModeTabs.BUILDING_BLOCKS);
-//	}
-//
-//	@SafeVarargs
-//	public static <B extends Block> RegistryObject<B> createBlock(String name, Supplier<? extends B> supplier, ResourceKey<CreativeModeTab>... group) {
-//		RegistryObject<B> block = APBlocks.BLOCKS.register(name, supplier);
-//		ForgeRegistryHelper.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-//		if (group != null) CreativeModeTabEventHandler.assignItemToTab(block, group);
-//		return block;
-//	}
-//
-//	public static <B extends Block> RegistryObject<B> createBlockNoItem(String name, Supplier<? extends B> supplier) {
-//		return APBlocks.BLOCKS.register(name, supplier);
-//	}
-//
-//
+    @SafeVarargs
+    public static Supplier<Block> createBlock(String name, Supplier<Block> block, @Nullable ResourceKey<CreativeModeTab>... tab) {
+        Supplier<Block> regBlock = Services.REGISTRY.registerBlock(name, block);
+        createItem(name, () -> new BlockItem(regBlock.get(), new Item.Properties()), tab);
+        return block;
+    }
+
+    public static Supplier<Block> createBlockNoItem(String name, Supplier<Block> block) {
+        return Services.REGISTRY.registerBlock(name, block);
+    }
+
 //	public static <B extends Block> StoneBlockSet createBoardSet(String name, Supplier<? extends B> supplier) {
 //		StoneBlockSet boardSet = new StoneBlockSet(createBlock(name, supplier), StoneBlockSet.SetGroup.NO_WALLS).woodify();
 //		boardSet.forEachRegistryObject((obj) -> ModelBakeEventHandler.register(obj, model -> new BoardModel(model, SpriteShift.getShift("block/" + name, "block/" + name + "_odd"))));
 //		return boardSet;
 //	}
+
 }
