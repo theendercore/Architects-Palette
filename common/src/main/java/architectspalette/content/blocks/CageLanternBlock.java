0140f1ebd23de85a7c8fde28152fb6f0209f688f
theendercore <theendercore@gmail.com>
2024-06-25 14:09:03 +0300

    - moved CageLanternBlock to common

--- common/src/main/java/architectspalette/content/blocks/CageLanternBlock.java
+++ common/src/main/java/architectspalette/content/blocks/CageLanternBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
