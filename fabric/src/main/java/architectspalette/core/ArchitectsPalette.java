38e25bfe54b3ec1bca03637dd9899ba0fadc4c8e
theendercore <theendercore@gmail.com>
2024-06-26 15:40:26 +0300

    - Merged fabric clients in to single class (This is fine I checked my friends mod, and he does this, so it's fine)

--- fabric/src/main/java/architectspalette/core/ArchitectsPalette.java
+++ fabric/src/main/java/architectspalette/core/ArchitectsPalette.java
@@ -1,9 +1,14 @@
 package architectspalette.core;
 
+import architectspalette.content.particles.WizardParticle;
 import architectspalette.core.event.CreativeModeTabEventHandler;
+import architectspalette.core.registry.MiscRegistry;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.particle.FlameParticle;
 
-public class ArchitectsPalette implements ModInitializer {
+public class ArchitectsPalette implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
@@ -13,5 +18,17 @@ public class ArchitectsPalette implements ModInitializer {
         APCommon.init();
 
         CreativeModeTabEventHandler.init();
+    }
+
+    @Override
+    public void onInitializeClient() {
+        APCommonClient.init();
+
+        registerParticleFactories();
+    }
+
+    public static void registerParticleFactories() {
+        ParticleFactoryRegistry.getInstance().register(MiscRegistry.GREEN_FLAME.get(), FlameParticle.Provider::new);
+        ParticleFactoryRegistry.getInstance().register(MiscRegistry.WIZARDLY_DEFENSE_BLAST.get(), WizardParticle.Provider::new);
     }
 }
