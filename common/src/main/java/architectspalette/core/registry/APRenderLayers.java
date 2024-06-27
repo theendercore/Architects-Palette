9a4d830b23ecb22b2ce697d3507d96a7577d5995
theendercore <theendercore@gmail.com>
2024-06-27 19:04:56 +0300

    - made APRenderLayers nicer (I know this is subjective, but it at lest less code)

--- common/src/main/java/architectspalette/core/registry/APRenderLayers.java
+++ common/src/main/java/architectspalette/core/registry/APRenderLayers.java
@@ -5,37 +5,27 @@ import architectspalette.core.platform.Services;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
+import java.util.stream.Stream;
+
 import static architectspalette.core.registry.APBlocks.*;
 
 public class APRenderLayers {
 
-    // (ender) Thanks for not doing a forgeism, this works better on multi-platform
     //"use json for it" yeah nice try forge. i know you won't take this out.
     //if you add dynamic render layers, then we'll talk.
+    // (ender) Thanks for not doing a forgeism, this works better on multi-platform
     public static void setupRenderLayers() {
-        Services.REGISTRY.setRenderLayer(ENTWINE_BARS, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(SUNMETAL_BARS, RenderType.cutout());
-
-        Services.REGISTRY.setRenderLayer(TWISTED_DOOR , RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(TWISTED_TRAPDOOR , RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(TWISTED_SAPLING , RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(POTTED_TWISTED_SAPLING , RenderType.cutout());
-
-        Services.REGISTRY.setRenderLayer(REDSTONE_CAGE_LANTERN, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(GLOWSTONE_CAGE_LANTERN, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(ALGAL_CAGE_LANTERN, RenderType.cutout());
-
-        Services.REGISTRY.setRenderLayer(ACACIA_TOTEM_WING, RenderType.cutout());
+        Stream.of(
+                ENTWINE_BARS, SUNMETAL_BARS,
+                TWISTED_DOOR, TWISTED_TRAPDOOR, TWISTED_SAPLING, POTTED_TWISTED_SAPLING,
+                REDSTONE_CAGE_LANTERN, GLOWSTONE_CAGE_LANTERN, ALGAL_CAGE_LANTERN,
+                ACACIA_TOTEM_WING,
+                NETHER_BRASS_FIRE, NETHER_BRASS_TORCH, NETHER_BRASS_WALL_TORCH, NETHER_BRASS_CHAIN, NETHER_BRASS_LANTERN
+        ).forEach((it) -> Services.REGISTRY.setRenderLayer(it, RenderType.cutout()));
 
         Services.REGISTRY.setRenderLayer(HELIODOR_ROD, RenderType.translucent());
         Services.REGISTRY.setRenderLayer(EKANITE_ROD, RenderType.translucent());
         Services.REGISTRY.setRenderLayer(MONAZITE_ROD, RenderType.translucent());
-
-        Services.REGISTRY.setRenderLayer(NETHER_BRASS_FIRE, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(NETHER_BRASS_TORCH, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(NETHER_BRASS_WALL_TORCH, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(NETHER_BRASS_CHAIN, RenderType.cutout());
-        Services.REGISTRY.setRenderLayer(NETHER_BRASS_LANTERN, RenderType.cutout());
 
         //I'm lazy
         // (ender) no you are a programmer.
