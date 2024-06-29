bfed1e9740b823db2411c01f398b125e93711bc1
theendercore <theendercore@gmail.com>
2024-06-29 16:43:20 +0300

    - inti new data-gen

--- fabric/src/main/java/architectspalette/core/ArchitectsPalette.java
+++ fabric/src/main/java/architectspalette/core/ArchitectsPalette.java
@@ -1,16 +1,21 @@
 package architectspalette.core;
 
 import architectspalette.content.particles.WizardParticle;
+import architectspalette.core.datagen.BlockTagProvider;
+import architectspalette.core.datagen.ItemTagProvider;
 import architectspalette.core.event.CreativeModeTabEventHandler;
 import architectspalette.core.integration.APVerticalSlabsCondition;
 import architectspalette.core.registry.MiscRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-//import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.client.particle.FlameParticle;
 
-public class ArchitectsPalette implements ModInitializer, ClientModInitializer {
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ArchitectsPalette implements ModInitializer, ClientModInitializer, DataGeneratorEntrypoint {
 
     @Override
     public void onInitialize() {
@@ -33,9 +38,17 @@ public class ArchitectsPalette implements ModInitializer, ClientModInitializer {
         ParticleFactoryRegistry.getInstance().register(MiscRegistry.WIZARDLY_DEFENSE_BLAST.get(), WizardParticle.Provider::new);
     }
 
-//    @Override
-//    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-//        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-//        pack.addProvider(BlockTagProvider::new);
-//    }
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator gen) {
+        FabricDataGenerator.Pack pack = gen.createPack();
+
+        // (ender) No, I don't think there is a nicer way of doing this :(
+        AtomicReference<BlockTagProvider> blockTags = new AtomicReference<>();
+        pack.addProvider((o, r) -> {
+            blockTags.set(new BlockTagProvider(o, r));
+            return blockTags.get();
+        });
+
+        pack.addProvider((o, r) -> new ItemTagProvider(o, r, blockTags.get()));
+    }
 }
