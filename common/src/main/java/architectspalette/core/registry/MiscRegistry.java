c23bf54bee4f73eb4d75c6d1abb8cc3bbfcf14c2
theendercore <theendercore@gmail.com>
2024-06-29 16:44:20 +0300

    - ItemTagProvider yippee

--- common/src/main/java/architectspalette/core/registry/MiscRegistry.java
+++ common/src/main/java/architectspalette/core/registry/MiscRegistry.java
@@ -4,6 +4,7 @@ import architectspalette.core.platform.Services;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 import java.util.function.Supplier;
@@ -18,9 +19,13 @@ public class MiscRegistry {
     public static final TagKey<Block> GREEN_FIRE_SUPPORTING = blockTag("green_fire_supporting");
     public static final TagKey<Block> WIZARD_BLOCKS = blockTag("wizard_blocks");
     public static final TagKey<Block> NUBS = blockTag("nubs");
+    public static final TagKey<Item> NUBS_ITEM = itemTag("nubs");
 
     public static TagKey<Block> blockTag(String name) {
         return TagKey.create(Registries.BLOCK, modLoc(name));
+    }
+    public static TagKey<Item> itemTag(String name) {
+        return TagKey.create(Registries.ITEM, modLoc(name));
     }
 
     //Particles
---
+++ fabric/src/main/java/architectspalette/core/datagen/ItemTagProvider.java
@@ -0,0 +1,40 @@
+package architectspalette.core.datagen;
+
+import architectspalette.core.registry.MiscRegistry;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ItemTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture, architectspalette.core.datagen.BlockTagProvider btp) {
+        super(output, registriesFuture, btp);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider wrapperLookup) {
+        copyBlockTags();
+    }
+
+    private void copyBlockTags() {
+        copy(BlockTags.WALLS, ItemTags.WALLS);
+        copy(BlockTags.SLABS, ItemTags.SLABS);
+        copy(BlockTags.STAIRS, ItemTags.STAIRS);
+        copy(MiscRegistry.NUBS, MiscRegistry.NUBS_ITEM);
+    }
+
+
+    @SafeVarargs
+    private FabricTagProvider<Item>.FabricTagBuilder getOrCreateTag(TagKey<Item> tagKey, Supplier<Item>... suppliers) {
+        var tag = getOrCreateTagBuilder(tagKey);
+        Arrays.stream(suppliers).map(Supplier::get).forEach(tag::add);
+        return tag;
+    }
+}
