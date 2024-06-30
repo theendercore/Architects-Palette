594ea60e83aac963614049a38ef1d2088d996d5b
theendercore <theendercore@gmail.com>
2024-06-30 20:12:09 +0300

    - item tags :) :gun:

--- fabric/src/main/java/architectspalette/core/datagen/ItemTagProvider.java
+++ fabric/src/main/java/architectspalette/core/datagen/ItemTagProvider.java
@@ -24,12 +24,31 @@ public class ItemTagProvider extends FabricTagProvider.ItemTagProvider {
     }
 
     private void copyBlockTags() {
+        // (ender) "tags will be simple", yeah right smartass
         copy(BlockTags.WALLS, ItemTags.WALLS);
         copy(BlockTags.SLABS, ItemTags.SLABS);
         copy(BlockTags.STAIRS, ItemTags.STAIRS);
+        copy(BlockTags.FENCES, ItemTags.FENCES);
+
+        copy(BlockTags.WOODEN_FENCES, ItemTags.WOODEN_FENCES);
+        copy(BlockTags.WOODEN_SLABS, ItemTags.WOODEN_SLABS);
+        copy(BlockTags.WOODEN_STAIRS, ItemTags.WOODEN_STAIRS);
+        copy(BlockTags.WOODEN_DOORS, ItemTags.WOODEN_DOORS);
+        copy(BlockTags.WOODEN_TRAPDOORS, ItemTags.WOODEN_TRAPDOORS);
+        copy(BlockTags.WOODEN_BUTTONS, ItemTags.WOODEN_BUTTONS);
+        copy(BlockTags.WOODEN_PRESSURE_PLATES, ItemTags.WOODEN_PRESSURE_PLATES);
+        copy(BlockTags.PLANKS, ItemTags.PLANKS);
+        copy(BlockTags.FENCE_GATES, ItemTags.FENCE_GATES);
+
+        copy(BlockTags.SAPLINGS, ItemTags.SAPLINGS);
+        copy(BlockTags.LEAVES, ItemTags.LEAVES);
+
+        copy(APTags.TWISTED_LOGS, APTags.TWISTED_LOGS_ITEM);
+        copy(BlockTags.LOGS_THAT_BURN, ItemTags.LOGS_THAT_BURN);
+
         copy(APTags.NUBS, APTags.NUBS_ITEM);
         copy(APTags.COPPER_NUBS, APTags.COPPER_NUBS_ITEM);
-        copy(APTags.TWISTED_LOGS, APTags.TWISTED_LOGS_ITEM);
+
         copy(APTags.ABYSSALINE, APTags.ABYSSALINE_ITEM);
         copy(APTags.HADALINE, APTags.HADALINE_ITEM);
     }
