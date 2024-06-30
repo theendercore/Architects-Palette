2c5225824284181dfa1571c371b700261c7c856b
theendercore <theendercore@gmail.com>
2024-06-30 16:00:42 +0300

    - moved KeyMaker to common

--- common/src/main/java/architectspalette/core/util/KeyMaker.java
+++ common/src/main/java/architectspalette/core/util/KeyMaker.java
@@ -3,9 +3,11 @@ package architectspalette.core.util;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
+import static architectspalette.core.APConstants.mcLoc;
 import static architectspalette.core.APConstants.modLoc;
 
 public class KeyMaker {
@@ -15,6 +17,10 @@ public class KeyMaker {
 
     public static ResourceKey<ConfiguredFeature<?, ?>> configuredFeature(String name) {
         return key(Registries.CONFIGURED_FEATURE, name);
+    }
+
+    public static ResourceKey<CreativeModeTab> vanillaTab(String name) {
+        return ResourceKey.create(Registries.CREATIVE_MODE_TAB, mcLoc(name));
     }
 
 
