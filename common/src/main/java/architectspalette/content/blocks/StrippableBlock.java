f45855dd43c93fecfed64b7a4926d50e5cd92283
theendercore <theendercore@gmail.com>
2024-06-30 15:11:05 +0300

    - removed unused code

--- common/src/main/java/architectspalette/content/blocks/StrippableBlock.java
+++ common/src/main/java/architectspalette/content/blocks/StrippableBlock.java
@@ -7,18 +7,15 @@ import net.minecraft.world.level.block.state.properties.Property;
 import java.util.Map;
 
 public interface StrippableBlock {
-    //(ender) that that java, "oh that's not safe 🤓". yeah, right I know what im doing
+    //(ender) shut up java, "oh that's not safe 🤓". yeah, right I know what im doing
     @SuppressWarnings("unchecked")
     default <T extends Comparable<T>> BlockState getStripedBlockState(Map<Property<?>, Comparable<?>> oldState) {
-        if (oldState == null) return getStripToBlock().defaultBlockState();
-        else {
-            BlockState block = getStripToBlock().defaultBlockState();
-            for (Map.Entry<Property<?>, Comparable<?>> entry : oldState.entrySet()) {
-                if (block.hasProperty(entry.getKey()))
-                    block = block.setValue((Property<T>) entry.getKey(), (T) entry.getValue());
-            }
-            return block;
+        BlockState block = getStripToBlock().defaultBlockState();
+        for (Map.Entry<Property<?>, Comparable<?>> entry : oldState.entrySet()) {
+            if (block.hasProperty(entry.getKey()))
+                block = block.setValue((Property<T>) entry.getKey(), (T) entry.getValue());
         }
+        return block;
     }
 
     Block getStripToBlock();
