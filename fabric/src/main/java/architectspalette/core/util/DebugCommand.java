b04020045fdf6ae2a8e52946972e756caafa4009
theendercore <theendercore@gmail.com>
2024-11-11 01:21:41 +0200

    - updated debug command

--- fabric/src/main/java/architectspalette/core/util/DebugCommand.java
+++ fabric/src/main/java/architectspalette/core/util/DebugCommand.java
@@ -1,11 +1,13 @@
 package architectspalette.core.util;
 
+import architectspalette.content.blocks.*;
 import architectspalette.core.platform.Services;
+import architectspalette.core.registry.APBlocks;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.*;
 
 import java.util.Objects;
 
@@ -26,8 +28,26 @@ public interface DebugCommand {
         var height = 0;
         for (Block block : Services.REGISTRY.getModBlocks()) {
             var pos = origin.east(row).north(height).above(height);
-            world.setBlock(pos.below(), Blocks.LIGHT_GRAY_CONCRETE.defaultBlockState(), Block.UPDATE_ALL);
-            world.setBlock(pos, block.defaultBlockState(), Block.UPDATE_ALL);
+            var ground = Blocks.LIGHT_GRAY_CONCRETE.defaultBlockState();
+            if (block instanceof SaplingBlock) ground = Blocks.GRASS_BLOCK.defaultBlockState();
+            else if (block instanceof GreenFireBlock) ground = APBlocks.NETHER_BRASS.get().defaultBlockState();
+            world.setBlock(pos.below(), ground, Block.UPDATE_ALL);
+
+            var state = block.defaultBlockState();
+            if (block instanceof WallTorchBlock || block instanceof TotemWingBlock || block instanceof ButtonBlock || block instanceof TotemBlock) {
+                state = state.setValue(WallTorchBlock.FACING, Direction.SOUTH);
+            }
+            switch (block) {
+                case VerticalSlabBlock i ->
+                        state = state.setValue(VerticalSlabBlock.TYPE, VerticalSlabBlock.VerticalSlabType.SOUTH);
+                case NubBlock i -> state = state.setValue(NubBlock.FACING, Direction.DOWN);
+                case CageLanternBlock i ->
+                        state = state.setValue(CageLanternBlock.INVERTED, true).setValue(CageLanternBlock.LIT, true);
+                default -> {
+                }
+            }
+
+            world.setBlock(pos, state, Block.UPDATE_ALL);
 
             if (row > 24) {
                 height++;
