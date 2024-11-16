f7edc1787c8233b7f63bfff21688b91fa4a4688e
TheEnderCore <theendercore@gmail.com>
2024-11-16 19:08:44 +0200

    - NeoRegistryHelper

--- neoforge/src/main/java/architectspalette/core/ArchitectsPalette.java
+++ neoforge/src/main/java/architectspalette/core/ArchitectsPalette.java
@@ -1,16 +1,46 @@
 package architectspalette.core;
 
 
-import net.minecraft.world.level.block.StairBlock;
+import architectspalette.core.platform.NeoForgeRegistryHelper;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
-@Mod(APConstants.MOD_ID)
+import static architectspalette.core.APConstants.MOD_ID;
+
+@Mod(MOD_ID)
 public class ArchitectsPalette {
 
-    public ArchitectsPalette(IEventBus eventBus) {
-        // Use NeoForge to bootstrap the Common mod.
-        APConstants.LOGGER.info("Hello NeoForge world!");
-//        APCommon.init();
+    public ArchitectsPalette(IEventBus modBus, ModContainer container) {
+        APCommon.initConfig();
+
+        APCommon.init();
+        NeoForgeRegistryHelper.register(modBus);
+
+        modBus.addListener(EventPriority.LOWEST, this::setupCommon);
+        modBus.addListener(EventPriority.LOWEST, this::setupClient);
+
+     /*   var LOOT = DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MOD_ID);
+        LOOT.register("wither_skeleton_bones", WitheredBoneLootModifier.CODEC);
+        LOOT.register(modEventBus);
+        // Biomes need to be registered before features.
+        APBiomeModifiers.BIOME_MODIFIER_SERIALIZER.register(modEventBus);
+
+        APVerticalSlabsCondition.registerCondition(modEventBus);*/
+
+//        NeoForge.EVENT_BUS.register(this);
+    }
+
+    void setupCommon(final FMLCommonSetupEvent event) {
+        // (ender) Thanks neoforge :)
+        APCommon.lateInit();
+    }
+
+    void setupClient(final FMLClientSetupEvent event) {
+        APCommon.initClient();
     }
 }
