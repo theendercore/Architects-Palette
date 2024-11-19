7af4a79b1485612dde434a7b9f378a3127a70ae1
TheEnderCore <theendercore@gmail.com>
2024-11-19 20:43:22 +0200

    - loot api v3

--- fabric/src/main/java/architectspalette/core/event/ModifyLootTableEventHandler.java
+++ fabric/src/main/java/architectspalette/core/event/ModifyLootTableEventHandler.java
@@ -2,13 +2,13 @@ package architectspalette.core.event;
 
 import architectspalette.core.registry.APItems;
 import architectspalette.core.util.ItemReplacer;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Items;
 
 public class ModifyLootTableEventHandler {
     public static void modify() {
-        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, lookup) -> {
             if (key == EntityType.WITHER_SKELETON.getDefaultLootTable()) {
                 tableBuilder.modifyPools(original -> {
                             ItemReplacer.replaceItem(original, Items.BONE, APItems.WITHERED_BONE.get());
